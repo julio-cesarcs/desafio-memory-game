520ba1ff1fc31aa6a50e68a553d492a5d374c376
jcoii39 <julio_cezzar@yahho.com.br>
2023-11-23 19:56:43 -0300

    inclusão dos arquivos

---
+++ src/scripts/engine.js
@@ -0,0 +1,58 @@
+const emojis = [
+    "🔥",
+    "🔥",
+    "⚡",
+    "⚡",
+    "🚗",
+    "🚗",
+    "🍔",
+    "🍔",
+    "⚽",
+    "⚽",
+    "😎",
+    "😎",
+    "🐶",
+    "🐶",
+    "🎯",
+    "🎯"
+];
+
+let openCards = [];
+
+let shuffleEmojis = emojis.sort(() => (Math.random() > 0.5 ? 2 : -1));
+
+for (let i = 0; i < emojis.length; i++) {
+    let box = document.createElement("div");
+    box.className = "item";
+    box.innerHTML = shuffleEmojis[i];
+    box.onclick = handeClick;
+    document.querySelector(".game").appendChild(box);
+}
+
+function handeClick() {
+    if (openCards.length < 2) {
+        this.classList.add("boxOpen");
+        openCards.push(this);
+    }
+
+    if (openCards.length == 2) {
+        setTimeout(checkMatch, 500);
+    }
+}
+
+function checkMatch() {
+    if (openCards[0].innerHTML === openCards[1].innerHTML) {
+        openCards[0].classList.add("boxMatch");
+        openCards[1].classList.add("boxMatch");
+    } else {
+        openCards[0].classList.remove("boxOpen");
+        openCards[1].classList.remove("boxOpen");
+    }
+
+    openCards = []
+
+    if(document.querySelectorAll(".boxMatch").length === emojis.length){
+        alert("Parabéns!!! Você venceu!!!")
+    }
+
+}
